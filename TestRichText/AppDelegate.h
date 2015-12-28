5595cc3484d8fba7db3ad6b61e86eec9e50078f9
HeJianghao <jianghaocool2006@163.com>
2015-12-28 17:28:44 +0800

    Initial Commit

---
+++ TestRichText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRichText
+//
+//  Created by 何江浩 on 15/12/28.
+//  Copyright © 2015年 何江浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestRichText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRichText
+//
+//  Created by 何江浩 on 15/12/28.
+//  Copyright © 2015年 何江浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
